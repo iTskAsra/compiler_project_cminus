c236f83882200eda6a057be265e60614b506aeb4
kasraamani <kasra138079@gmail.com>
2021-10-24 01:10:12 +0330

    Corrected scanner code.

--- scanner.py
+++ scanner.py
@@ -1,3 +1,16 @@
-tokens_add = "tokens.txt"
-lex_errors = "lexical_errors.txt"
-symbol_table = ""
+tokens_addr = "tokens.txt"
+lex_errors_addr = "lexical_errors.txt"
+symbol_table_addr = "symbol_table.txt"
+input_addr = "input.txt"
+current_line = 0
+symtab = "1.    if\n" \
+         "2.    else\n" \
+         "3.    void\n" \
+         "4.    int\n" \
+         "5.    repeat\n" \
+         "6.    break\n" \
+         "7.    until\n" \
+         "8.    return\n" \
+         "9.    main\n"
+
+
